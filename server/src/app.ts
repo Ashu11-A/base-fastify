07b937e4036865b493a52f40a3121c11d911cdba
Ashu <Matheusn.Biolowons@gmail.com>
2025-03-25 16:41:54 -0300

    📈 Boot server by cluster

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,9 @@ import 'env/loader'
 import 'dotenv/config'
 import 'reflect-metadata'
 
-import { execSync } from 'child_process'
 import { Fastify } from './controllers/fastify.js'
 import { Router } from './controllers/router.js'
 import Database from './database/dataSource.js'
-
-execSync('bun run migration:run || true', { stdio: 'inherit' })
 
 const fastify = new Fastify({ port: Number(process.env['PORT']) || 3000, host: '0.0.0.0' })
 await Database.initialize()
